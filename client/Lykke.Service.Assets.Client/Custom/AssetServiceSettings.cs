3cf1b8fadbc62dc27193820900d76b3d108e6ff4
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-08-30 12:10:25 +0300

    settings dto added

--- client/Lykke.Service.Assets.Client/Custom/AssetServiceSettings.cs
+++ client/Lykke.Service.Assets.Client/Custom/AssetServiceSettings.cs
@@ -3,6 +3,9 @@ using System.Net.Http;
 
 namespace Lykke.Service.Assets.Client.Custom
 {
+    /// <summary>
+    /// Itended to use in <see cref="ServiceCollectionExtensions.UseAssetsClient"/>
+    /// </summary>
     public class AssetServiceSettings
     {
         public Uri BaseUri { get; set; }
@@ -15,6 +18,16 @@ namespace Lykke.Service.Assets.Client.Custom
             return new AssetServiceSettings
             {
                 BaseUri = baseUri,
+                AssetsCacheExpirationPeriod = cacheExpirationPeriod,
+                AssetPairsCacheExpirationPeriod = cacheExpirationPeriod
+            };
+        }
+
+        public static AssetServiceSettings Create(AssetsSettings settings, TimeSpan cacheExpirationPeriod)
+        {
+            return new AssetServiceSettings
+            {
+                BaseUri = new Uri(settings.ServiceUrl),
                 AssetsCacheExpirationPeriod = cacheExpirationPeriod,
                 AssetPairsCacheExpirationPeriod = cacheExpirationPeriod
             };
---
+++ client/Lykke.Service.Assets.Client/Custom/AssetsSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Lykke.Service.Assets.Client.Custom
+{
+    /// <summary>
+    /// Inteded to use in app settings class. Class's property which contains this settings, should be named Assets
+    /// </summary>
+    public class AssetsSettings
+    {
+        public string ServiceUrl { get; set; }
+    }
+}
