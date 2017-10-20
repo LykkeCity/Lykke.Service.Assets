a96f18a9b40a2bd765d17705879a32fe99e7137a
Yury Glushkov <mail@glushkov.us>
2017-10-20 14:18:01 +0300

    Creation of assets for erc20 tokens improved

--- src/Lykke.Service.Assets.Services/Erc20TokenAssetService.cs
+++ src/Lykke.Service.Assets.Services/Erc20TokenAssetService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Lykke.Service.Assets.Core.Domain;
@@ -25,31 +26,80 @@ namespace Lykke.Service.Assets.Services
 
         public async Task<IAsset> CreateAssetForErc20TokenAsync(string tokenAddress)
         {
-            var erc20Token        = Mapper.Map<Erc20Token>(await _erc20TokenService.GetByTokenAddressAsync(tokenAddress));
-            var asset             = Mapper.Map<Asset>(_assetService.CreateDefault());
-            var assetExtendedInfo = Mapper.Map<AssetExtendedInfo>(_assetExtendedInfoService.CreateDefault());
-            
+            var erc20Token = Mapper.Map<Erc20Token>(await _erc20TokenService.GetByTokenAddressAsync(tokenAddress));
 
-            asset.Blockchain = Blockchain.Ethereum;
-            asset.Id         = Guid.NewGuid().ToString();
-            asset.IsDisabled = true;
-            asset.Name       = $"Asset for erc20 token {erc20Token.Address}";
-            asset.Type       = AssetType.Erc20Token;
+            if (string.IsNullOrEmpty(erc20Token.AssetId))
+            {
+                throw new InvalidOperationException($"Asset for the token with specified address [{tokenAddress}] already exists.");
+            }
+
+            // Creating asset
+            var asset                   = Mapper.Map<Asset>(_assetService.CreateDefault());
+            var blockchainAndDisplayIds = await GetBlockchainAndDisplayIdsAsync(erc20Token);
+            var tokenDecimals           = erc20Token.TokenDecimals ?? 0;
+
+            asset.Accuracy          = tokenDecimals;
+            asset.AssetAddress      = erc20Token.Address;
+            asset.Blockchain        = Blockchain.Ethereum;
+            asset.BlockChainId      = blockchainAndDisplayIds.BlockchainId;
+            asset.BlockChainAssetId = erc20Token.Address;
+            asset.DisplayId         = blockchainAndDisplayIds.DisplayId;
+            asset.Id                = Guid.NewGuid().ToString();
+            asset.IsDisabled        = true;
+            asset.MultiplierPower   = tokenDecimals;
+            asset.Name              = GetAssetName(erc20Token);
+            asset.Symbol            = erc20Token.TokenSymbol;
+            asset.Type              = AssetType.Erc20Token;
 
             asset = Mapper.Map<Asset>(await _assetService.AddAsync(asset));
 
+            // Creating extended info
+            var assetExtendedInfo = Mapper.Map<AssetExtendedInfo>(_assetExtendedInfoService.CreateDefault());
 
-            assetExtendedInfo.Id = asset.Id;
+            assetExtendedInfo.Id            = asset.Id;
+            assetExtendedInfo.NumberOfCoins = erc20Token.TokenTotalSupply;
+            
 
             await _assetExtendedInfoService.AddAsync(assetExtendedInfo);
 
-
+            // Updating token
             erc20Token.AssetId = asset.Id;
 
             await _erc20TokenService.UpdateAsync(erc20Token);
 
-
+            
             return asset;
+        }
+
+        private static string GetAssetName(IErc20Token token)
+        {
+            return !string.IsNullOrEmpty(token.TokenName)
+                ? $"Erc20 token {token.TokenName} ({token.Address})"
+                : $"Erc20 token {token.Address}";
+        }
+
+        private async Task<(string BlockchainId, string DisplayId)> GetBlockchainAndDisplayIdsAsync(IErc20Token token)
+        {
+            var assets        = (await _assetService.GetAllAsync()).ToArray();
+            var blockchainIds = assets.Select(x => x.BlockChainId).ToArray();
+            var displayIds    = assets.Select(x => x.DisplayId).ToArray();
+
+            string GetUniqueId(string[] existingIds)
+            {
+                var uniqueId        = token.TokenSymbol;
+                var duplicatesCount = 0;
+
+                while (existingIds.Contains(uniqueId))
+                {
+                    duplicatesCount++;
+
+                    uniqueId = $"{token.TokenSymbol}_{duplicatesCount + 1}";
+                }
+
+                return uniqueId;
+            }
+
+            return (GetUniqueId(blockchainIds), GetUniqueId(displayIds));
         }
     }
 }
