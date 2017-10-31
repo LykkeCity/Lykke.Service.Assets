4cbf6556626c2004b2b8dcec9a01fa6077457ebb
Yury Glushkov <mail@glushkov.us>
2017-10-31 10:58:31 +0300

    Commented code removed

--- src/Lykke.Service.Assets.Core/Services/IAssetDescriptionService.cs
+++ src/Lykke.Service.Assets.Core/Services/IAssetDescriptionService.cs
@@ -5,20 +5,5 @@ namespace Lykke.Service.Assets.Core.Services
     public interface IAssetDescriptionService
     {
         IAssetDescription CreateDefault();
-
-        //public static AssetDescription CreateDefault(string id)
-        //{
-        //    return new AssetDescription
-        //    {
-        //        Id = id,
-        //        PopIndex = 0,
-        //        MarketCapitalization = string.Empty,
-        //        Description = string.Empty,
-        //        IssuerName = string.Empty,
-        //        AssetClass = string.Empty,
-        //        NumberOfCoins = string.Empty,
-        //        AssetDescriptionUrl = string.Empty
-        //    };
-        //}
     }
 }
