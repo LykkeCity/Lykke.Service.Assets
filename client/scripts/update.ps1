1ac87cca12edfea8ecbf41a13c54d36a1f0d1761
Yury Glushkov <mail@glushkov.us>
2017-10-31 10:58:59 +0300

    Unused files removed

--- client/scripts/update.ps1
+++
@@ -1,3 +0,0 @@
-cd ./client/Lykke.Service.Assets.Client/
-iwr http://localhost:5000/swagger/v1/swagger.json -o Service.Assets.json
-autorest --input-file=Service.Assets.json --csharp --namespace=Lykke.Service.Assets.Client --output-folder=./
