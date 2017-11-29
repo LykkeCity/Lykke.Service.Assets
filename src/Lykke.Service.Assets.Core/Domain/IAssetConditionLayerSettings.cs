58430c1e7f43aafcfc28c6480a382f809b53b149
Novichikhin Alexey <fieldid@gmail.com>
2017-11-29 21:05:46 +0700

    add lost files

---
+++ src/Lykke.Service.Assets.Core/Domain/IAssetConditionLayerSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Lykke.Service.Assets.Core.Domain
+{
+    /// <summary>
+    /// Settings to all asset in layer
+    /// </summary>
+    public interface IAssetConditionLayerSettings
+    {
+        /// <summary>
+        /// Enabled or Disables cashin via back card for all asserst
+        /// </summary>
+        bool? ClientsCanCashInViaBankCards { get; }
+
+        /// <summary>
+        /// Enabled or Disables cashin swift for all asserst
+        /// </summary>
+        bool? SwiftDepositEnabled { get; }
+    }
+}
