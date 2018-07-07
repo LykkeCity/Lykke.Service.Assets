b0288c3ab30f500bfc67f2c717af710fe5a133b0
AleksandrBelousov <tortyt1@gmail.com>
2018-07-08 00:25:38 +0400

    Added Tool to add erc2- tokens and related assets

---
+++ src/RegisterErc20Tokens/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace RegisterErc20Tokens
+{
+    public static class ExceptionExtensions
+    {
+        public static string ToOperationResult(this Exception e)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("exception caught");
+            sb.AppendLine("-----");
+            sb.AppendLine();
+            sb.AppendLine(e.ToString());
+            sb.AppendLine();
+            sb.AppendLine("-----");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/RegisterErc20Tokens/Specification.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RegisterErc20Tokens
+{
+    public class Specification
+    {
+        public IEnumerable<string>Groups { get; set; }
+
+        public IEnumerable<ContractMetadata> Tokens { get; set; }
+    }
+}
