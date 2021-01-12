4a155de60841c21af176e31a4c6559615d026dd9
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-01-12 18:31:17 +0200

    missing files

---
+++ tests/Lykke.Service.Assets.Tests.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lykke.Service.Assets.Tests.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
