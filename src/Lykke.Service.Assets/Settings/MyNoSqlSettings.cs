bbf4763dda7ae6c4fb4f05af7783fb4fd6a66095
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-01-19 14:05:09 +0200

    new settings is optional

--- src/Lykke.Service.Assets/Settings/MyNoSqlSettings.cs
+++ src/Lykke.Service.Assets/Settings/MyNoSqlSettings.cs
@@ -1,8 +1,12 @@
-﻿namespace Lykke.Service.Assets.Settings
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Service.Assets.Settings
 {
     public class MyNoSqlSettings
     {
         public string WriterServiceUrl { get; set; }
+
+        [Optional]
         public int MaxClientsInCache { get; set; } = 10000;
     }
 }
